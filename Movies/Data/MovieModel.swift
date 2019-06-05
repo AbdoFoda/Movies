d2dc9f05be690febbe8bc1347ed5e5ea1e3ae2a9
Abdulrahman Sobbhy <abdo@Abdulrahmans-MacBook-Pro.local>
2019-06-05 11:53:46 +0200

    initial project Structure

---
+++ Movies/Data/MovieModel.swift
@@ -0,0 +1,71 @@
+//
+//  MovieModel.swift
+//  Movies
+//
+//  Created by Abdulrahman Sobbhy on 6/5/19.
+//  Copyright © 2019 Abdulrahman Sobbhy. All rights reserved.
+//
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let movie = try? newJSONDecoder().decode(Movie.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Movie
+struct Movie: Codable {
+    let movies: [MovieElement]
+}
+
+// MARK: - MovieElement
+struct MovieElement: Codable {
+    let title: String
+    let year: Int
+    let cast: [String]
+    let genres: [Genre]
+    let rating: Int
+}
+
+enum Genre: String, Codable {
+    case action = "Action"
+    case adventure = "Adventure"
+    case animated = "Animated"
+    case biography = "Biography"
+    case comedy = "Comedy"
+    case crime = "Crime"
+    case dance = "Dance"
+    case disaster = "Disaster"
+    case documentary = "Documentary"
+    case drama = "Drama"
+    case erotic = "Erotic"
+    case family = "Family"
+    case fantasy = "Fantasy"
+    case featuresTheOrganization = "Features the organization"
+    case foundFootage = "Found Footage"
+    case historical = "Historical"
+    case horror = "Horror"
+    case independent = "Independent"
+    case legal = "Legal"
+    case liveAction = "Live Action"
+    case martialArts = "Martial Arts"
+    case musical = "Musical"
+    case mystery = "Mystery"
+    case noir = "Noir"
+    case performance = "Performance"
+    case political = "Political"
+    case romance = "Romance"
+    case satire = "Satire"
+    case scienceFiction = "Science Fiction"
+    case slasher = "Slasher"
+    case sports = "Sports"
+    case spy = "Spy"
+    case superhero = "Superhero"
+    case supernatural = "Supernatural"
+    case suspense = "Suspense"
+    case teen = "Teen"
+    case thriller = "Thriller"
+    case war = "War"
+    case western = "Western"
+}
+
