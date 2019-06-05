559cae64899ea6e8635e56f4dbdf6813e5d0b3bc
Abdulrahman Sobbhy <abdo@Abdulrahmans-MacBook-Pro.local>
2019-06-05 19:32:29 +0200

    initialize FlickerURL for image loading within Alamofire

---
+++ Movies/Data/FlickerURL.swift
@@ -0,0 +1,9 @@
+//
+//  FlickerURL.swift
+//  Movies
+//
+//  Created by Abdulrahman Sobbhy on 6/5/19.
+//  Copyright © 2019 Abdulrahman Sobbhy. All rights reserved.
+//
+
+import Foundation
---
+++ Movies/Data/MoviePhotosModel.swift
@@ -0,0 +1,9 @@
+//
+//  MoviePhotosModel.swift
+//  Movies
+//
+//  Created by Abdulrahman Sobbhy on 6/5/19.
+//  Copyright © 2019 Abdulrahman Sobbhy. All rights reserved.
+//
+
+import Foundation
